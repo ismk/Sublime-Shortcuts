37c2627821f3a6536c8e78c69e14f98dc1ba9e58
Ismail <ismailkalimi@gmail.com>
2014-08-05 12:06:18 -0500

    Initial Commit, Added files for Exercise

---
+++ Cursor_Movement.rb
@@ -0,0 +1,10 @@
+CURSOR MOVEMENT
+_______________
+
+ACTION                                                           KEYSTROKE
+======                                                          ===========
+  Move Left / Right                                             arrow(u/d/l/r)
+
+Move Extreme Up/Down/Left/ Right                            cmd + arrow(u/d/l/r)
+
+Move One word                                                   option + arrow(l/r)
