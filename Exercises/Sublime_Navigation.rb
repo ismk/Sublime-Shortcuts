5e4784386496cbe162969884dc234fb218d0b210
Ismail <ismailkalimi@gmail.com>
2014-08-05 12:16:49 -0500

    Modified / Cleaned up the files

---
+++ Exercises/Sublime_Navigation.rb
@@ -0,0 +1,12 @@
+SUBLIME NAVIGATION
+__________________
+
+ACTION              											 KEYSTROKE
+======																		===========
+*Switch Tabs 													command + option + arrow(l/r)
+
+*Search Bar                							 command + p
+
+Select Quadrant 													control + [1,2,3,4]
+
+Move File to Quadrant									control + shift + [1,2,3,4]
